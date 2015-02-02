36456ef45e011ce13b393721babbcc264e52da88
Chris McCandlish <cmccand@gmail.com>
2015-02-02 00:08:55 -0500

    committing updated code so both of our branches match

--- main.js
+++ main.js
@@ -48,397 +48,397 @@ var movies = {
 
 
 
-renderDysfunct: function (dysfunct, index, array) {
-  dysfunct.idx = index;
-  var compiledDysfunct = _.template(templates.movieData);
-  $('.dysfunctional').append(compiledDysfunct(dysfunct));
+  renderDysfunct: function (dysfunct, index, array) {
+    dysfunct.idx = index;
+    var compiledDysfunct = _.template(templates.movieData);
+    $('.dysfunctional').append(compiledDysfunct(dysfunct));
 
 
-},
+  },
 
-renderAllDysfunct: function (dysfunctInfo) {
-  dysfunctInfo.forEach(movies.renderDysfunct);
+  renderAllDysfunct: function (dysfunctInfo) {
+    dysfunctInfo.forEach(movies.renderDysfunct);
 
-},
+  },
 
-renderMustFind: function (mustFind, index, array) {
-  mustFind.idx = index;
-  var compiledMustFind = _.template(templates.movieData);
-  $('.misplaced').append(compiledMustFind(mustFind));
+  renderMustFind: function (mustFind, index, array) {
+    mustFind.idx = index;
+    var compiledMustFind = _.template(templates.movieData);
+    $('.misplaced').append(compiledMustFind(mustFind));
 
 
-},
+  },
 
-renderAllMustFind: function (MustFindInfo) {
-  MustFindInfo.forEach(movies.renderMustFind);
+  renderAllMustFind: function (MustFindInfo) {
+    MustFindInfo.forEach(movies.renderMustFind);
 
-},
+  },
 
-renderDog: function (Dog, index, array) {
-  Dog.idx = index;
-  var compiledDog = _.template(templates.movieData);
-  $('.dogEscapes').append(compiledDog(Dog));
+  renderDog: function (Dog, index, array) {
+    Dog.idx = index;
+    var compiledDog = _.template(templates.movieData);
+    $('.dogEscapes').append(compiledDog(Dog));
 
 
-},
+  },
 
-renderAllDogs: function (DogInfo) {
-  DogInfo.forEach(movies.renderDog);
+  renderAllDogs: function (DogInfo) {
+    DogInfo.forEach(movies.renderDog);
 
-},
+  },
 
-renderBelieve: function (Believe, index, array) {
-  Believe.idx = index;
-  var compiledBelieve = _.template(templates.movieData);
-  $('.justBelieve').append(compiledBelieve(Believe));
+  renderBelieve: function (Believe, index, array) {
+    Believe.idx = index;
+    var compiledBelieve = _.template(templates.movieData);
+    $('.justBelieve').append(compiledBelieve(Believe));
 
 
-},
+  },
 
-renderAllBelieve: function (BelieveInfo) {
-  BelieveInfo.forEach(movies.renderBelieve);
+  renderAllBelieve: function (BelieveInfo) {
+    BelieveInfo.forEach(movies.renderBelieve);
 
-},
+  },
 
-renderCancer: function (cancer, index, array) {
-  cancer.idx = index;
-  var compiledCancer = _.template(templates.movieData);
-  $('.cancer').append(compiledCancer(cancer));
+  renderCancer: function (cancer, index, array) {
+    cancer.idx = index;
+    var compiledCancer = _.template(templates.movieData);
+    $('.cancer').append(compiledCancer(cancer));
 
 
-},
+  },
 
-renderAllCancer: function (CancerInfo) {
-  CancerInfo.forEach(movies.renderCancer);
+  renderAllCancer: function (CancerInfo) {
+    CancerInfo.forEach(movies.renderCancer);
 
-},
+  },
 
-renderFakeblood: function (Fakeblood, index, array) {
-  Fakeblood.idx = index;
-  var compiledFakeblood = _.template(templates.movieData);
-  $('.fakeBlood').append(compiledFakeblood(Fakeblood));
+  renderFakeblood: function (Fakeblood, index, array) {
+    Fakeblood.idx = index;
+    var compiledFakeblood = _.template(templates.movieData);
+    $('.fakeBlood').append(compiledFakeblood(Fakeblood));
 
 
-},
+  },
 
-renderAllFakeblood: function (FakebloodInfo) {
-  FakebloodInfo.forEach(movies.renderFakeblood);
+  renderAllFakeblood: function (FakebloodInfo) {
+    FakebloodInfo.forEach(movies.renderFakeblood);
 
-},
+  },
 
-renderPurple: function (Purple, index, array) {
-  Purple.idx = index;
-  var compiledPurple = _.template(templates.movieData);
-  $('.purple').append(compiledPurple(Purple));
+  renderPurple: function (Purple, index, array) {
+    Purple.idx = index;
+    var compiledPurple = _.template(templates.movieData);
+    $('.purple').append(compiledPurple(Purple));
 
-},
+  },
 
-renderAllPurple: function (PurpleInfo) {
-  PurpleInfo.forEach(movies.renderPurple);
+  renderAllPurple: function (PurpleInfo) {
+    PurpleInfo.forEach(movies.renderPurple);
 
-},
+  },
 
-renderDrunk: function (Drunk, index, array) {
-  Drunk.idx = index;
-  var compiledDrunk = _.template(templates.movieData);
-  $('.famousWriter').append(compiledDrunk(Drunk));
+  renderDrunk: function (Drunk, index, array) {
+    Drunk.idx = index;
+    var compiledDrunk = _.template(templates.movieData);
+    $('.famousWriter').append(compiledDrunk(Drunk));
 
 
-},
+  },
 
-renderAllDrunk: function (DrunkInfo) {
-  DrunkInfo.forEach(movies.renderDrunk);
+  renderAllDrunk: function (DrunkInfo) {
+    DrunkInfo.forEach(movies.renderDrunk);
 
-},
+  },
 
-renderSuck: function (youSuck, index, array) {
-  youSuck.idx = index;
-  var compiledYouSuck = _.template(templates.movieData);
-  $('.youSuck').append(compiledYouSuck(youSuck));
+  renderSuck: function (youSuck, index, array) {
+    youSuck.idx = index;
+    var compiledYouSuck = _.template(templates.movieData);
+    $('.youSuck').append(compiledYouSuck(youSuck));
 
 
-},
+  },
 
-renderAllSuck: function (SuckInfo) {
-  SuckInfo.forEach(movies.renderSuck);
+  renderAllSuck: function (SuckInfo) {
+    SuckInfo.forEach(movies.renderSuck);
 
-},
+  },
 
-renderForties: function (Forties, index, array) {
-  Forties.idx = index;
-  var compiledForties = _.template(templates.movieData);
-  $('.witty').append(compiledForties(Forties));
+  renderForties: function (Forties, index, array) {
+    Forties.idx = index;
+    var compiledForties = _.template(templates.movieData);
+    $('.witty').append(compiledForties(Forties));
 
 
-},
+  },
 
-renderAllForties: function (FortiesInfo) {
-  FortiesInfo.forEach(movies.renderForties);
+  renderAllForties: function (FortiesInfo) {
+    FortiesInfo.forEach(movies.renderForties);
 
-},
+  },
 
-renderHappy: function (happySince12, index, array) {
-  happySince12.idx = index;
-  var compiledHappy = _.template(templates.movieData);
-  $('.happySince12').append(compiledHappy(happySince12));
+  renderHappy: function (happySince12, index, array) {
+    happySince12.idx = index;
+    var compiledHappy = _.template(templates.movieData);
+    $('.happySince12').append(compiledHappy(happySince12));
 
-},
+  },
 
-renderAllHappy: function (HappyInfo) {
-HappyInfo.forEach(movies.renderHappy);
+  renderAllHappy: function (HappyInfo) {
+    HappyInfo.forEach(movies.renderHappy);
 
-},
+  },
 
-rendermaid: function (maid, index, array) {
-  maid.idx = index;
-  var compiledMaid = _.template(templates.movieData);
-  $('.hotelMaid').append(compiledMaid(maid));
+  rendermaid: function (maid, index, array) {
+    maid.idx = index;
+    var compiledMaid = _.template(templates.movieData);
+    $('.hotelMaid').append(compiledMaid(maid));
 
-},
+  },
 
-renderAllmaid: function (maidInfo) {
-  maidInfo.forEach(movies.rendermaid);
+  renderAllmaid: function (maidInfo) {
+    maidInfo.forEach(movies.rendermaid);
 
-},
+  },
 
-renderVDay: function (vDay, index, array) {
-  vDay.idx = index;
-  var compiledVDay = _.template(templates.movieData);
-  $('.vDay').append(compiledVDay(vDay));
+  renderVDay: function (vDay, index, array) {
+    vDay.idx = index;
+    var compiledVDay = _.template(templates.movieData);
+    $('.vDay').append(compiledVDay(vDay));
 
-},
+  },
 
-renderAllVDay: function (vDayInfo) {
-  vDayInfo.forEach(movies.renderVDay);
+  renderAllVDay: function (vDayInfo) {
+    vDayInfo.forEach(movies.renderVDay);
 
-},
+  },
 
-renderDeadDog: function (deadDog, index, array) {
-  deadDog.idx = index;
-  var compiledDeadDog = _.template(templates.movieData);
-  $('.dog').append(compiledDeadDog(deadDog));
+  renderDeadDog: function (deadDog, index, array) {
+    deadDog.idx = index;
+    var compiledDeadDog = _.template(templates.movieData);
+    $('.dog').append(compiledDeadDog(deadDog));
 
-},
+  },
 
-renderAllDeadDog: function (deadDogInfo) {
-  deadDogInfo.forEach(movies.renderDeadDog);
+  renderAllDeadDog: function (deadDogInfo) {
+    deadDogInfo.forEach(movies.renderDeadDog);
 
-},
+  },
 
-renderBitter: function (bitter, index, array) {
-  bitter.idx = index;
-  var compiledBitter = _.template(templates.movieData);
-  $('.bitter').append(compiledBitter(bitter));
+  renderBitter: function (bitter, index, array) {
+    bitter.idx = index;
+    var compiledBitter = _.template(templates.movieData);
+    $('.bitter').append(compiledBitter(bitter));
 
-},
+  },
 
-renderAllBitter: function (bitterInfo) {
-  bitterInfo.forEach(movies.renderBitter);
+  renderAllBitter: function (bitterInfo) {
+    bitterInfo.forEach(movies.renderBitter);
 
-},
+  },
 
-renderRudy: function (Rudy, index, array) {
-  Rudy.idx = index;
-  var compiledRudy = _.template(templates.movieData);
-  $('.rudySacks').append(compiledRudy(Rudy));
+  renderRudy: function (Rudy, index, array) {
+    Rudy.idx = index;
+    var compiledRudy = _.template(templates.movieData);
+    $('.rudySacks').append(compiledRudy(Rudy));
 
-},
+  },
 
-renderAllRudy: function (RudyInfo) {
-  RudyInfo.forEach(movies.renderRudy);
+  renderAllRudy: function (RudyInfo) {
+    RudyInfo.forEach(movies.renderRudy);
 
-},
+  },
 
-renderBitter: function (bitter, index, array) {
-  bitter.idx = index;
-  var compiledBitter = _.template(templates.movieData);
-  $('.bitter').append(compiledBitter(bitter));
+  renderBitter: function (bitter, index, array) {
+    bitter.idx = index;
+    var compiledBitter = _.template(templates.movieData);
+    $('.bitter').append(compiledBitter(bitter));
 
-},
+  },
 
-renderAllBitter: function (bitterInfo) {
-  bitterInfo.forEach(movies.renderBitter);
+  renderAllBitter: function (bitterInfo) {
+    bitterInfo.forEach(movies.renderBitter);
 
-},
+  },
 
-renderTheme: function (Theme, index, array) {
-  Theme.idx = index;
-  var compiledTheme = _.template(templates.movieData);
-  $('.themeMusic').append(compiledTheme(Theme));
+  renderTheme: function (Theme, index, array) {
+    Theme.idx = index;
+    var compiledTheme = _.template(templates.movieData);
+    $('.themeMusic').append(compiledTheme(Theme));
 
-},
+  },
 
-renderAllTheme: function (ThemeInfo) {
-  ThemeInfo.forEach(movies.renderTheme);
+  renderAllTheme: function (ThemeInfo) {
+    ThemeInfo.forEach(movies.renderTheme);
 
-},
+  },
 
-renderAsshole: function (asshole, index, array) {
-  asshole.idx = index;
-  var compiledAsshole = _.template(templates.movieData);
-  $('.assholeMakeHistory').append(compiledAsshole(asshole));
+  renderAsshole: function (asshole, index, array) {
+    asshole.idx = index;
+    var compiledAsshole = _.template(templates.movieData);
+    $('.assholeMakeHistory').append(compiledAsshole(asshole));
 
-},
+  },
 
-renderAllAsshole: function (AssholeInfo) {
-  AssholeInfo.forEach(movies.renderAsshole);
+  renderAllAsshole: function (AssholeInfo) {
+    AssholeInfo.forEach(movies.renderAsshole);
 
-},
+  },
 
-renderPrison: function (Prison, index, array) {
-  Prison.idx = index;
-  var compiledPrison = _.template(templates.movieData);
-  $('.survivePrison').append(compiledPrison(Prison));
+  renderPrison: function (Prison, index, array) {
+    Prison.idx = index;
+    var compiledPrison = _.template(templates.movieData);
+    $('.survivePrison').append(compiledPrison(Prison));
 
-},
+  },
 
-renderAllPrison: function (PrisonInfo) {
-  PrisonInfo.forEach(movies.renderPrison);
+  renderAllPrison: function (PrisonInfo) {
+    PrisonInfo.forEach(movies.renderPrison);
 
-},
+  },
 
-renderGenius: function (genius, index, array) {
-  genius.idx = index;
-  var compiledGenius = _.template(templates.movieData);
-  $('.genius').append(compiledGenius(genius));
+  renderGenius: function (genius, index, array) {
+    genius.idx = index;
+    var compiledGenius = _.template(templates.movieData);
+    $('.genius').append(compiledGenius(genius));
 
-},
+  },
 
-renderAllGenius: function (GeniusInfo) {
-  GeniusInfo.forEach(movies.renderGenius);
+  renderAllGenius: function (GeniusInfo) {
+    GeniusInfo.forEach(movies.renderGenius);
 
-},
+  },
 
-renderNazi: function (nazi, index, array) {
-  nazi.idx = index;
-  var compiledNazi = _.template(templates.movieData);
-  $('.nazi').append(compiledNazi(nazi));
+  renderNazi: function (nazi, index, array) {
+    nazi.idx = index;
+    var compiledNazi = _.template(templates.movieData);
+    $('.nazi').append(compiledNazi(nazi));
 
-},
+  },
 
-renderAllNazi: function (NaziInfo) {
-  NaziInfo.forEach(movies.renderNazi);
+  renderAllNazi: function (NaziInfo) {
+    NaziInfo.forEach(movies.renderNazi);
 
-},
+  },
 
-renderSplatter: function (splatter, index, array) {
-  splatter.idx = index;
-  var compiledSplatter = _.template(templates.movieData);
-  $('.splatter').append(compiledSplatter(splatter));
+  renderSplatter: function (splatter, index, array) {
+    splatter.idx = index;
+    var compiledSplatter = _.template(templates.movieData);
+    $('.splatter').append(compiledSplatter(splatter));
 
-},
+  },
 
-renderAllSplatter: function (SplatterInfo) {
-  SplatterInfo.forEach(movies.renderSplatter);
+  renderAllSplatter: function (SplatterInfo) {
+    SplatterInfo.forEach(movies.renderSplatter);
 
-},
+  },
 
-renderHurt: function (hurt, index, array) {
-  hurt.idx = index;
-  var compiledHurt = _.template(templates.movieData);
-  $('.hurtYourself').append(compiledHurt(hurt));
+  renderHurt: function (hurt, index, array) {
+    hurt.idx = index;
+    var compiledHurt = _.template(templates.movieData);
+    $('.hurtYourself').append(compiledHurt(hurt));
 
-},
+  },
 
-renderAllHurt: function (HurtInfo) {
-  HurtInfo.forEach(movies.renderHurt);
+  renderAllHurt: function (HurtInfo) {
+    HurtInfo.forEach(movies.renderHurt);
 
-},
+  },
 
-renderCrisis: function (crisis, index, array) {
-  crisis.idx = index;
-  var compiledCrisis = _.template(templates.movieData);
-  $('.midlifeCrisis').append(compiledCrisis(crisis));
+  renderCrisis: function (crisis, index, array) {
+    crisis.idx = index;
+    var compiledCrisis = _.template(templates.movieData);
+    $('.midlifeCrisis').append(compiledCrisis(crisis));
 
-},
+  },
 
-renderAllCrisis: function (CrisisInfo) {
-  CrisisInfo.forEach(movies.renderCrisis);
+  renderAllCrisis: function (CrisisInfo) {
+    CrisisInfo.forEach(movies.renderCrisis);
 
-},
+  },
 
-renderSarcasm: function (sarcasm, index, array) {
-  sarcasm.idx = index;
-  var compiledSarcasm = _.template(templates.movieData);
-  $('.sarcasm').append(compiledSarcasm(sarcasm));
+  renderSarcasm: function (sarcasm, index, array) {
+    sarcasm.idx = index;
+    var compiledSarcasm = _.template(templates.movieData);
+    $('.sarcasm').append(compiledSarcasm(sarcasm));
 
-},
+  },
 
-renderAllSarcasm: function (SarcasmInfo) {
-  SarcasmInfo.forEach(movies.renderSarcasm);
+  renderAllSarcasm: function (SarcasmInfo) {
+    SarcasmInfo.forEach(movies.renderSarcasm);
 
-},
+  },
 
-renderBanned: function (banned, index, array) {
-  banned.idx = index;
-  var compiledBanned = _.template(templates.movieData);
-  $('.banned').append(compiledBanned(banned));
+  renderBanned: function (banned, index, array) {
+    banned.idx = index;
+    var compiledBanned = _.template(templates.movieData);
+    $('.banned').append(compiledBanned(banned));
 
-},
+  },
 
-renderAllBanned: function (BannedInfo) {
-  BannedInfo.forEach(movies.renderBanned);
+  renderAllBanned: function (BannedInfo) {
+    BannedInfo.forEach(movies.renderBanned);
 
-},
+  },
 
-renderChopEm: function (chopEm, index, array) {
-  chopEm.idx = index;
-  var compiledChopEm = _.template(templates.movieData);
-  $('.chopEm').append(compiledChopEm(chopEm));
+  renderChopEm: function (chopEm, index, array) {
+    chopEm.idx = index;
+    var compiledChopEm = _.template(templates.movieData);
+    $('.chopEm').append(compiledChopEm(chopEm));
 
-},
+  },
 
-renderAllChopEm: function (ChopEmInfo) {
-  ChopEmInfo.forEach(movies.renderChopEm);
+  renderAllChopEm: function (ChopEmInfo) {
+    ChopEmInfo.forEach(movies.renderChopEm);
 
-},
+  },
 
-renderIncest: function (incest, index, array) {
-  incest.idx = index;
-  var compiledIncest = _.template(templates.movieData);
-  $('.incest').append(compiledIncest(incest));
+  renderIncest: function (incest, index, array) {
+    incest.idx = index;
+    var compiledIncest = _.template(templates.movieData);
+    $('.incest').append(compiledIncest(incest));
 
-},
+  },
 
-renderAllIncest: function (IncestInfo) {
-  IncestInfo.forEach(movies.renderIncest);
+  renderAllIncest: function (IncestInfo) {
+    IncestInfo.forEach(movies.renderIncest);
 
-},
+  },
 
-renderDeadDog2: function (deadDog2, index, array) {
-  deadDog2.idx = index;
-  var compiledDeadDog2 = _.template(templates.movieData);
-  $('.dog2').append(compiledDeadDog2(deadDog2));
+  renderDeadDog2: function (deadDog2, index, array) {
+    deadDog2.idx = index;
+    var compiledDeadDog2 = _.template(templates.movieData);
+    $('.dog2').append(compiledDeadDog2(deadDog2));
 
-},
+  },
 
-renderAllDeadDog2: function (deadDogInfo2) {
-  deadDogInfo2.forEach(movies.renderDeadDog2);
+  renderAllDeadDog2: function (deadDogInfo2) {
+    deadDogInfo2.forEach(movies.renderDeadDog2);
 
-},
+  },
 
-renderScarred: function (scarred, index, array) {
-  scarred.idx = index;
-  var compiledScarred = _.template(templates.movieData);
-  $('.scarred').append(compiledScarred(scarred));
+  renderScarred: function (scarred, index, array) {
+    scarred.idx = index;
+    var compiledScarred = _.template(templates.movieData);
+    $('.scarred').append(compiledScarred(scarred));
 
-},
+  },
 
-renderAllScarred: function (ScarredInfo) {
-  ScarredInfo.forEach(movies.renderScarred);
+  renderAllScarred: function (ScarredInfo) {
+    ScarredInfo.forEach(movies.renderScarred);
 
-},
-renderFoodSex: function (foodSex, index, array) {
-  foodSex.idx = index;
-  var compiledfoodSex = _.template(templates.movieData);
-  $('.foodSex').append(compiledfoodSex(foodSex));
+  },
+  renderFoodSex: function (foodSex, index, array) {
+    foodSex.idx = index;
+    var compiledfoodSex = _.template(templates.movieData);
+    $('.foodSex').append(compiledfoodSex(foodSex));
 
-},
+  },
 
-renderAllFoodSex: function (foodSexInfo) {
-  foodSexInfo.forEach(movies.renderFoodSex);
+  renderAllFoodSex: function (foodSexInfo) {
+    foodSexInfo.forEach(movies.renderFoodSex);
 
-}
+  }
 
 
 };
@@ -471,7 +471,7 @@ $(document).ready(function(){
 
   });
 
-$('.subMood h2').hide();
+  $('.subMood h2').hide();
 
 
 
--- templates.js
+++ templates.js
@@ -3,9 +3,6 @@ var templates = {};
 templates.movieData = [
 
 '<div class="resultBox active">',
-'<img src=<%= Poster %> />',
-'<div class="resultText">',
-'<h5><%= Title %></h5>',
 '<h4><%= Title %></h4>',
 '<img src=<%= Poster %> />',
 '<p><b>Year Released:</b> <%= Year %></p>',
